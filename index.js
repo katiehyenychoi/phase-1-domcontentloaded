db17044fa3f177a594d88c563db27716320304e4
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-09 12:32:01 -0400

    commit tree f2e2ab36353ee086e7ee5d0820deebcc0919a150

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    let parag = document.querySelector("#text");
+    parag.innerHTML = "This is really cool!"
+});
+
+
+
